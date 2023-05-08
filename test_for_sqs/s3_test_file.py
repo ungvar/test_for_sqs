b11f259a7349fddd8c1f54912ce58063ce89fcbf
ivanselivanov <ivan@selivanov.ws>
2023-05-08 22:36:48 +0300

    Научился загружать и скачивать файлы в / из корзины S3

--- test_for_sqs/s3_test_file.py
+++ test_for_sqs/s3_test_file.py
@@ -2,8 +2,16 @@ import boto3  # pip install boto3
 
 # Let's use Amazon S3
 session = boto3.Session(profile_name='test_user')
-s3 = session.resource('s3')
+s3 = session.client('s3')
 
-# Print out bucket names
-for bucket in s3.buckets.all():
-    print(bucket.name)
+
+s3.upload_file('string.txt', 'test-bucket-for-selivaka', 'hello.txt')
+
+with open('byte.txt', 'rb') as data:
+    s3.upload_fileobj(data, 'test-bucket-for-selivaka', 'mykey')
+
+
+s3.download_file('test-bucket-for-selivaka', 'hello.txt', 'string.txt')
+
+with open('byte.txt', 'wb') as data:
+    s3.download_fileobj('test-bucket-for-selivaka', 'mykey', data)
